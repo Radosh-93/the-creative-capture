5c5d21a682775bce5cbd21051cfce9165c0f92c9
Radosh-93 <051217al>
2020-11-14 19:55:14 +0200

    styled Services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,58 +1,36 @@
 import React from 'react';
 import home1 from '../img/home1.png'
 import styled from 'styled-components'
+import {Wrapper, Description, Hide, Image} from "../styles";
 
 const AboutSection = (props) => {
 	return (
-		<About>
-			<Description>
-				<div className="title">
-					<Hide>
-						<h2>We work to make</h2>
-					</Hide>
-					<Hide>
-						<h2>your <span>dreams</span></h2>
-					</Hide>
-					<Hide>
-						<h2>come true.</h2>
-					</Hide>
-				</div>
-				<p>Contact us for any photography or videography ideas that you have. We have professionals with amazing
-					skills. </p>
-				<button>Contact Us</button>
-			</Description>
-			<Image>
-				<img src={home1} alt="guy with a camera"/>
-			</Image>
-		</About>
+		<div className={'about'}>
+			<Wrapper>
+				<Description>
+					<div className="title">
+						<Hide>
+							<h2>We work to make</h2>
+						</Hide>
+						<Hide>
+							<h2>your <span>dreams</span></h2>
+						</Hide>
+						<Hide>
+							<h2>come true.</h2>
+						</Hide>
+					</div>
+					<p>Contact us for any photography or videography ideas that you have. We have professionals with
+						amazing
+						skills. </p>
+					<button>Contact Us</button>
+				</Description>
+				<Image>
+					<img src={home1} alt="guy with a camera"/>
+				</Image>
+			</Wrapper>
+		</div>
 	);
 };
 //Styled Components
-const About = styled.div`
-	min-height: 90vh;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 5rem 10rem;
-	color: white;
-`;
-const Description = styled.div`
-	flex: 1;
-	padding-right: 5rem;
-	h2 {
-		font-weight: lighter;
-	}
-`;
-const Image = styled.div`
-	flex: 1;
-	overflow: hidden;
-	img {
-		width: 100%;
-		height: 80vh;
-		object-fit: cover;
-	}
-`;
-const Hide = styled.div`
-	overflow: hidden;
-`
+
 export default AboutSection;
