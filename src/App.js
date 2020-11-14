e151aa87a23e75786bf5eb0ed60060cf015a6e82
Radosh-93 <051217al>
2020-11-14 18:21:00 +0200

    styled About section, added global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+        <GlobalStyles/>
         <AboutUs/>
     </div>
   );
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,51 @@
+import {createGlobalStyle} from "styled-components";
+
+const greenColor = '#23d997';
+
+const GlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    background-color: #1b1b1b;
+}
+button {
+    font-weight: bold;
+    font-size: 1.1rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid ${greenColor};
+    background-color: transparent;
+    color: white;
+    transition: all .3s ease;
+    &:hover {
+        background-color: ${greenColor};
+    }
+}
+h2 {
+	font-weight: lighter;
+	font-size: 4rem;
+}
+h3 {
+	color: #ffffff;
+}
+h4 {
+	font-weight: bold;
+}
+a {
+	font-size: 1.1rem;
+}
+span {
+	font-weight: bold;
+	color: ${greenColor};
+}
+p {
+	padding: 3rem 0;
+	color:#ccc;
+	font-size: 1.4rem;
+	line-height: 1.5;
+}
+`
+export default GlobalStyles;
