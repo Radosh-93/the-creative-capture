6d0dcdd275ca09dbdfc50ddfba5561ada5b6a74e
Radosh-93 <051217al>
2020-11-14 20:09:41 +0200

    styled Faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,76 @@
 import React from 'react';
+import styled from 'styled-components'
+import {Wrapper} from "../styles";
 
 const FaqSection = (props) => {
-    return (
-        <div className={'faq'}>
-            <h2>Any Question <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
+	return (
+		<div className={'faq'}>
+			<Faq>
+				<h2>Any Question <span>FAQ</span></h2>
+				<div className="question">
+					<h4>How Do I Start?</h4>
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
 
-                </div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
+					</div>
+					<div className="faq-line"/>
+				</div>
+				<div className="question">
+					<h4>Daily Schedule</h4>
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
 
-                </div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
+					</div>
+					<div className="faq-line"/>
+				</div>
+				<div className="question">
+					<h4>Different Payment Methods</h4>
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
 
-                </div>
-            </div>
-            <div className="question">
-                <h4>What Products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
-
-                </div>
-            </div>
-        </div>
-    );
+					</div>
+					<div className="faq-line"/>
+				</div>
+				<div className="question">
+					<h4>What Products do you offer?</h4>
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, molestiae?</p>
+					</div>
+					<div className="faq-line"/>
+				</div>
+			</Faq>
+		</div>
+	);
 };
-
+const Faq = styled(Wrapper)`
+	display: block;
+	span {
+		display: block;
+	}
+	h2 {
+	padding-bottom: 2rem;
+	font-weight: lighter;
+	}
+	.faq-line {
+		background-color: #cccccc;
+		height: 0.2rem;
+		margin: 2rem 0;
+		width: 100%;
+	}
+	.question {
+		padding: 3rem 0;
+		cursor: pointer;
+		.answer {
+		padding: 2rem 0;
+		p {
+		padding: 1rem 0;
+		}
+		}
+	}
+	
+`;
 export default FaqSection;
